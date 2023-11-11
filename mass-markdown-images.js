d560f8115e3ffd3d7f7ab09daf3bf8ac3f6cb550
Dan DiGangi <me@dandigangi.com>
2023-11-11 14:08:55 -0600

    cars problem w/ 2 solutions

--- mass-markdown-images.js
+++ mass-markdown-images.js
@@ -4,9 +4,12 @@
 const max = 73;
 let strs = '';
 
+// Numbers were specific to how many photos I talk so 1-41 first and then uploaded day 2 which was 42+
 for (let i = 42; i < max; i++) {
   strs += `![LeadDev West Coast Photos - ${i}](/static/images/blog/events/leaddev-west-oct-23/leaddev-west-oct-23-${i}.png)\n\n`;
 }
 
 console.log(strs);
+
+// Wasn't worth doing this in the end since I could push it to the console and copy/paste it out
 // fs.writeFile('./imageMd.txt');
