b1f7f27a5585dbefb82465fe94e9fc2f89ef9906
Dan DiGangi <me@dandigangi.com>
2024-01-31 23:00:38 -0600

    file dump scripts

--- scripts.sh
+++ scripts.sh
@@ -4,3 +4,9 @@ for entry in "$dir"/*
 do
   echo "$entry"
 done
+
+# Dump VS Code extensions list to text file
+code --list-extensions > vs-code-extensions.txt
+
+# Dump Homebrew formulas to text file
+brew list --full-name > homebrew-formulas.txt
